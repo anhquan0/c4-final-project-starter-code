a0a9f9a50c1e535716840696db056211b8ed8467
thanmahethong1622 <thanmahethong1622@gmail.com>
2023-11-22 13:55:00 +0700

    generateUploadUrl

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -6,13 +6,19 @@ import { cors, httpErrorHandler } from 'middy/middlewares'
 
 import { createAttachmentPresignedUrl } from '../../service/todos'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('generateUpload');
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
     const userId = getUserId(event)
-    const uploadUrl = createAttachmentPresignedUrl(userId, todoId)
+
+    const uploadUrl = createAttachmentPresignedUrl(userId, todoId).then(url => {
+      logger.info('Upload URL:', url)
+    })
 
     return {
       statusCode: 200,
